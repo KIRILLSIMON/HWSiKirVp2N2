7e452fee22bccfc71d330aa4fe3078fa30ac2fba
sikir <sikir2022@gmail.com>
2022-11-10 15:11:27 +0300

    3+4 DZ

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+public class Main {
+    public static void main(String[] args) {
+    /*{System.out.println("Reshenie nachalmyh zadaniy");
+        int age=6;
+        if (age>=18){
+            System.out.println("S okonchaniem shkoly;Shuruy v VUZ");
+        } else if (age==24) {
+            System.out.println("Tuneyadstvu-BATLE!Seichas zhe pshel rabotat");
+
+        }else if (age>=7) {
+        System.out.println("Pervoklashkoy pora zadelatsya");
+    }   else {
+            System.out.println("A nu ynec galopiruy to DOU/ babysiter/");
+        }
+        System.out.println("от Симонова К.О.-ответ на ДЗ1 zadach1i2 ч.1 УО");
+   */
+        /*System.out.println("Reshenie 3go uprajneniya DZ1 UOp1");
+        int vmestimost = 103;
+        if (vmestimost <= 60) {
+            System.out.println("Kto uspel tot PRISEL");
+        } else if (vmestimost <= 102) {
+            System.out.println("Nu i stoy-pihaysysa v perepolnennom vagone");
+        } else
+            System.out.println("Grubo narushena technologia perevozok!Vinovniy poneset otvetstvennost!");*/
+/*System.out.println("Reshenie attrakcionnoy zadachi");
+int age=15;
+if (age<5) {
+    System.out.println("Attraction's ENTERTAINMENT/enjoyment/ is IMPOSSIBLE");
+        } else if (age<14) {
+    System.out.println("You are need babysiter");}
+        else if (age>14) {
+      System.out.println("Всё на качельках бакланишь дылда?Освой лучше paraglyding!");
+        }
+        *//* System.out.println("Zadacha7");
+        int nachalnoyeOzhirenyeInGr=7000;
+        int dietosportivnayaDnevnayaZhiroubilInGr=500;
+        if (dietosportivnayaDnevnayaZhiroubilInGr<=500 && dietosportivnayaDnevnayaZhiroubilInGr>=251)
+        {
+        int FizroStradanieInDays=nachalnoyeOzhirenyeInGr/dietosportivnayaDnevnayaZhiroubilInGr;
+        System.out.println("Не свихнулся ещё от физры?- вперёд: "+FizroStradanieInDays+"  дней тебе маяться");}
+        else if (dietosportivnayaDnevnayaZhiroubilInGr<=250){
+            int dietosportivnayaZhiroubil2InGr=250;
+            int fizroStradanie2=nachalnoyeOzhirenyeInGr/dietosportivnayaZhiroubil2InGr;
+            System.out.println(fizroStradanie2);*/
+
+       /* System.out.println("Zadacha8");
+        int ZPMashi = 67760;
+        int ZPDenisa = 83690;
+        int ZPKristiny = 76230;
+        int HNYnewZarplataM = ZPMashi / 10 + ZPMashi;
+        int HNYnewZarplataD = ZPDenisa / 10 + ZPDenisa;
+        int HNYnewZarplataK = ZPKristiny / 10 + ZPKristiny;
+        System.out.println(HNYnewZarplataM);
+        System.out.println(HNYnewZarplataD);
+        System.out.println(HNYnewZarplataK);
+        int PrezhGDohM = ZPMashi * 12;
+        int PrezhGDohD = ZPDenisa * 12;
+        int PrezhGDohK = ZPKristiny * 12;
+        int HNYDohM = HNYnewZarplataM * 12;
+        int HNYDohD = HNYnewZarplataD * 12;
+        int HNYDohK = HNYnewZarplataK * 12;
+        int PererashodDlyM = HNYDohM - PrezhGDohM;
+        int PererashodDlyD = HNYDohD - PrezhGDohD;
+        int PererashodDlyK = HNYDohK - PrezhGDohK;
+        System.out.println("Доп. убытки компании в рублИЩАХ отныне составят на:  ");
+        System.out.println("Марию  - " + PererashodDlyM);
+        System.out.println("Дениса  - " + PererashodDlyD);
+        System.out.println("Кристину  - " + PererashodDlyK);
+        System.out.println("P.S.: и не забудьте доп.перерасход на Корвалол/Валокардин-для главбуха с шефом!");*/
+System.out.println("Решение последней 3й задачи по ч.1 УО");
+int one=1;
+int two=2;
+int three=5;
+if (one>two&&one>three){
+    System.out.println(one);
+} else if (two>one&&two>three) {
+    System.out.println(two);
+}else if (three>one&&three>two) {
+    System.out.println(three);}
+    }
+}
+
+
+
